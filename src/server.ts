0e26befb9c1b90785d0f16aa1004478713e821da
Victor Nogueira <vgdsno@gmail.com>
2026-05-07 13:36:25 -0300

    chore: adiciona container de banco de dados mysql e dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 
+require("dotenv").config();
+
+
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
